80d44dd50a88328c1479d110b548e4e50496b02c
Dwan13 <dwanveloza@gmail.com>
2018-04-17 16:51:12 -0500

    Terminado 2

--- lexer.py
+++ lexer.py
@@ -1,6 +1,6 @@
 import ply.lex as lex
 
-tokens = ['FINSI','FINPARA','ENTONCES','FOR','IF','DO']+[ 'NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS','LPAREN','RPAREN','MAYOR','MENOR','MAYORIGUAL','MENORIGUAL','DIFERENTE' ]
+tokens = ['FINSI','FINPARA','ENTONCES','FOR','IF','DO']+[ 'NAME','SALTO','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS','LPAREN','RPAREN','MAYOR','MENOR','MAYORIGUAL','MENORIGUAL','DIFERENTE' ]
 
 
 t_ignore = ' \t'
@@ -15,7 +15,7 @@ t_MENOR=r'<'
 t_MENORIGUAL=r'<='
 t_DIFERENTE=r'!='
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
+t_SALTO=r'\n '
 t_LPAREN =r'\('
 t_RPAREN =r'\)'
 
@@ -31,9 +31,13 @@ def t_IF(t):
     return t
 def t_ENTONCES(t):
     r'Entonces'
+    
     return t
 def t_FINSI(t):
     r'FinSi'
+    return t
+def t_FINPARA(t):
+    r'FinPara'
     return t
 def t_NUMBER(t):
     r'\d+'
@@ -41,15 +45,17 @@ def t_NUMBER(t):
     return t
 
 # Error handling rule
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+if __name__ == "__main__":
+    lex.lex() # Build the lexer
+    archivo = open("Expresiones2.txt", "r")
 
-lex.lex() # Build the lexer
-
-lex.input("x = 3 - 4 + 5 * 6 () Para Si > <= != Entonces entonces")
-while True:
-    tok = lex.token()
-    if not tok: break
-    print str(tok.value) + " - " + str(tok.type)
+    lex.input(archivo.read())
+    while True:
+        tok = lex.token()
+        if not tok: break
+        print str(tok.value) + " - " + str(tok.type)
